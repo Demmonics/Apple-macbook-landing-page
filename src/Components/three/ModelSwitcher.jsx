94b928c78ebc87312e3c43f28b5f9806fb282bcd
Demmonics <yooshaabbas2005@gmail.com>
2026-06-15 03:56:25 +0530

    Corrected bugs with the utility function to note for the future dont use = unless u want to asign otherwisse react is retarded and wont even tell u why it is not working only use = to assign a function or some shit idk at 4 am

--- src/Components/three/ModelSwitcher.jsx
+++ src/Components/three/ModelSwitcher.jsx
@@ -12,19 +12,19 @@ const OFFSET_DISTANCE=5;// this is how far one model will move off screen when i
 const fadeMeshes =(group,opacity) => {
     if(!group) return;
 
-    group.traverse((child)) => {
+    group.traverse((child) => {
         if(child.isMesh) {
             child.material.transparent = true; // we set the material to transparent at the start then we slowly animate it to make it come to life
             gsap.to(child.material, {opacity,duration:ANIMATION_DURATION})// animated slowly
         }
-    }
+    })
 }
 
 const moveGroup =(group , x) =>
 {
     if(!group) return;
 
-    gsap.to(group.position, P x,duration=ANIMATION_DURATION) // We are animating the horizontal moving duration of the macbook
+    gsap.to(group.position,  {x,duration:ANIMATION_DURATION}) // We are animating the horizontal moving duration of the macbook
 }
 
 const ModelSwitcher = ({scale, isMobile }) => {
@@ -39,18 +39,22 @@ const ModelSwitcher = ({scale, isMobile }) => {
         speed:1,
         zoom:1,
         polar: [-Math.PI,Math.PI],
-        azimuth: [-Infinity,Infinity]
-        config = {mass:1 , tension:0 ,friction:26}
+        azimuth: [-Infinity,Infinity],
+        config : {
+            mass:1 ,
+            tension:0 ,
+            friction:26
+        }
     }
 
     return(
         <>
-        <PresentationControls { ...controlsConfig}>
+        <PresentationControls {...controlsConfig}>
             <group ref={largeMacbookRef}>
                 <MacbookModel16 scale = {isMobile ? 0.05: 0.08} />
             </group>
         </PresentationControls>
-        <PresentationControls { ...controlsConfig}>
+        <PresentationControls {...controlsConfig}>
             <group ref={smallMacbookRef}>
                 <MacbookModel14 scale = {isMobile ? 0.03: 0.06} />
             </group>
@@ -64,3 +68,13 @@ export default ModelSwitcher;
 //polar: [-Math.PI,Math.PI] This adds rotation limits which helps us see below of the macbook when the user rotates the macbook
 //azimuith is the horizontal scroll which helps us set a restriction or remove restrictions from our scroll
 //config adds realistic phsyics with mass tension and friction it tries to imitate it best
+
+/*Rule to remember
+In an object:
+The parser is seeing:
+{
+    config = {
+
+and thinking:
+"Why are you assigning inside an object definition?"
+*/
