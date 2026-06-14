ca9989ee025177b85e241fb541a711e0c7cd6221
Demmonics <yooshaabbas2005@gmail.com>
2026-06-15 03:42:51 +0530

    Added the in animation of the macbook using gsap in ModelSwitcher.jsx

--- src/Components/ProductViewer.jsx
+++ src/Components/ProductViewer.jsx
@@ -56,7 +56,7 @@ const ProductViewer = () => {
         <Canvas id="canvas" camera = {{ position : [0,2,5], fov:50, near: 0.1, far:100 }}>
             <ambientLight intensity={1} />
             <Studiolight />
-            <MacbookModel14 scale ={0.06} position={[0,0,0]} />
+            
         <ModelSwitcher scale={isMobile ? scale - 0.03 : scale}  isMobile={isMobile}/>
         
         </Canvas>
--- src/Components/three/ModelSwitcher.jsx
+++ src/Components/three/ModelSwitcher.jsx
@@ -4,18 +4,46 @@ import { useRef } from "react";
 import MacbookModel from "../models/Macbook";
 import MacbookModel16 from "../models/Macbook-16";
 import MacbookModel14 from "../models/Macbook-14";
+import gsap from "gsap";
+
+const ANIMATION_DURATION =1;
+const OFFSET_DISTANCE=5;// this is how far one model will move off screen when its hidden
+
+const fadeMeshes =(group,opacity) => {
+    if(!group) return;
+
+    group.traverse((child)) => {
+        if(child.isMesh) {
+            child.material.transparent = true; // we set the material to transparent at the start then we slowly animate it to make it come to life
+            gsap.to(child.material, {opacity,duration:ANIMATION_DURATION})
+        }
+    }
+}
+
 const ModelSwitcher = ({scale, isMobile }) => {
+
     const smallMacbookRef = useRef();
     const largeMacbookRef = useRef();// these two r from react
+
     const showLargeMacbook = scale === 0.08 || scale === 0.05 ;
+
+    const controlsConfig ={
+        snap: true,
+        speed:1,
+        zoom:1,
+        polar: [-Math.PI,Math.PI],
+        azimuth: [-Infinity,Infinity]
+        config = {mass:1 , tension:0 ,friction:26}
+    }
+
     return(
         <>
-        <PresentationControls>
+        <PresentationControls { ...controlsConfig}>
             <group ref={largeMacbookRef}>
                 <MacbookModel16 scale = {isMobile ? 0.05: 0.08} />
             </group>
         </PresentationControls>
-        <PresentationControls>
+        <PresentationControls { ...controlsConfig}>
             <group ref={smallMacbookRef}>
                 <MacbookModel14 scale = {isMobile ? 0.03: 0.06} />
             </group>
@@ -25,4 +53,7 @@ const ModelSwitcher = ({scale, isMobile }) => {
     )
 }
 export default ModelSwitcher;
-// <> this indicates a react fragment 
+// <> this indicates a react fragment
+//polar: [-Math.PI,Math.PI] This adds rotation limits which helps us see below of the macbook when the user rotates the macbook
+//azimuith is the horizontal scroll which helps us set a restriction or remove restrictions from our scroll
+//config adds realistic phsyics with mass tension and friction it tries to imitate it best
