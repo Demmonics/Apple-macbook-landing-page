766b38008acacf5f54ea07dd84e3cb727bcb3d4d
Demmonics <yooshaabbas2005@gmail.com>
2026-06-14 15:28:38 +0530

    Added the gsap plug in scroll trigger and split text and registered it in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Hero from "./Components/hero"
 import NavBar from "./Components/NavBar"
 import ProductViewer from "./Components/ProductViewer"
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all"; {/*Scroll tigger lets us animate things like pinning scrubing and triggering based on the scroll trigger*/}
 
+gsap.registerPlugin{ScrollTrigger}{/*we do this cause gsap has to be registered to be used everywhere */}
 const App = () => {
   return (
     <main>
