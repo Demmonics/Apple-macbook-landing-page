eaa4427ba46c1276c27f67ac829809fbc314a236
Demmonics <yooshaabbas2005@gmail.com>
2026-06-17 22:18:04 +0530

    this time i saved before pushing :added the game video and the mask and a little bit of text but did not implement mask yet

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -7,6 +7,28 @@ const Showcase = (() => {
                     <img src = "/mask-logo.svg" />
                 </div>
             </div>
+            <div className="content">
+                <div className="wrapper">
+                    <div className="lg:max-w-md">
+                        {/* This will make sure it dosent go out of the screen and only takes max width*/}
+                        <h2>Rocket Chip</h2>
+
+                        <div classname="spcae-y-5 mt-7 pe-10">
+                            <p>
+                                Introducting{" "}
+                                <span className="text-white">
+                                    M4, the next generation of Apple Silicon
+                                </span>
+                                . M4 powers
+                            </p>
+                            <p>
+                                
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </section>
     )
 })
