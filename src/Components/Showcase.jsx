63eb6246d882e63049afb019193ea10f4ee87ee3
Demmonics <yooshaabbas2005@gmail.com>
2026-06-18 04:10:32 +0530

    added the showcase secction with scroll animation

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,6 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
+import gsap from 'gsap';
+
 const Showcase = (() => {
+    const isTablet = useMediaQuery({ query : '(max-width:1024px'})
+
+    useGSAP(()=> {
+        if(!isTablet) {
+            const timeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '#showcase',
+                    start: 'top top',
+                    end: 'bottom top',
+                    scrub: true,
+                    pin: true,
+
+                }
+            })
+            timeline.to('.mask img', {
+                transform: 'scale(1.1)'
+            }).to('.content',{opacity:1 , y:0, ease: 'powerl.in'});
+        }
+    },[isTablet])
     return(
-        <section id="Showcase">
+        <section id="showcase">
             <div className="media">
                 <video src="/videos/game.mp4" loop muted autoPlay playsInline />
                 <div className="mask">
@@ -13,7 +36,7 @@ const Showcase = (() => {
                         {/* This will make sure it dosent go out of the screen and only takes max width*/}
                         <h2>Rocket Chip</h2>
 
-                        <div classname="spcae-y-5 mt-7 pe-10">
+                        <div className="space-y-5 mt-7 pe-10">
                             <p>
                                 Introducting{" "}
                                 <span className="text-white">
@@ -22,10 +45,28 @@ const Showcase = (() => {
                                 . M4 powers
                             </p>
                             <p>
-                                
+                                It drives Apple Intelligence on iPad Pro, so you can write, create, and accomplish more with ease. All in a design that’s unbelievably thin, light, and powerful.
                             </p>
+                            <p>
+                                A brand-new display engine delivers breathtaking precision, color accuracy, and brightness. And a next-gen GPU with hardware-accelerated ray tracing brings console-level graphics to your fingertips.
+                            </p>
+                            <p className="text-primary">Learn more about Apple Intelligence</p>
                         </div>
 
+                    </div>
+                </div>
+                <div className="max-w-3xs space-y-14">
+                    <div className="space-y-2">
+                        <p> Up to</p>
+                        <h3>4x faster</h3>
+                        <p>pro rendering performance than M2</p>
+                    </div>
+                </div>
+                <div className="max-w-3xs space-y-14">
+                    <div className="space-y-2">
+                        <p> Up to</p>
+                        <h3>1.5x faster</h3>
+                        <p>CPU rendering performance than M2</p>
                     </div>
                 </div>
             </div>
