0f7d226ee4bdad473ee7cd85857534cad4d596bd
Demmonics <yooshaabbas2005@gmail.com>
2026-06-30 12:04:53 +0530

    Changed the hosting port(wont be shown as part of vite.config)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   server: {
+    host: '127.0.0.1',
+    port: 3000, // or 8080 — both clear of all your excluded ranges
+  },
 })
